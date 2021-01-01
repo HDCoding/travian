ecc4b46e8ccc52846139b39c10a6e5a6a98137ef
John <juaorok@hotmail.com>
2020-12-31 22:04:58 -0300

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -5,9 +5,9 @@
  */
 
 define('MAILER', 'smtp');
-define('SMTP_USERNAME', 'bd1693de1ed477');
-define('SMTP_PASSWORD', '123456789');
+define('SMTP_USERNAME', '');
+define('SMTP_PASSWORD', '');
 define('SMTP_PORT', 2525);
 define('SMTP_HOST', 'smtp.mailtrap.io');
 define('SMTP_ENCRYPTION', 'tls');
-define('FROM_MAIL', 'monill@inbox.mailtrap.io');
+define('FROM_MAIL', '');
