69a5de0a479b54fe22815e0ca0c0cf6e0ef46aa0
John <juaorok@hotmail.com>
2021-01-01 10:02:14 -0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -7,7 +7,8 @@
 // Website URL
 define('URL', 'http://travian.dev');
 // App root
-define('RUT', $_SERVER['DOCUMENT_ROOT']);
+define('DOC_ROOT', $_SERVER['DOCUMENT_ROOT']);
+define('LOG_PATH', DOC_ROOT . '/logs/');
 
 // Set timezone
 date_default_timezone_set('America/Sao_Paulo');
@@ -15,7 +16,7 @@ date_default_timezone_set('America/Sao_Paulo');
 error_reporting(E_ALL); //^ E_NOTICE
 ini_set("display_errors", true);
 ini_set("log_errors", true);
-ini_set("error_log", RUT . "/logs/php-error.txt");
+ini_set("error_log", LOG_PATH . "php-error.txt");
 
 // TRANSLATION
 define('DEFAULT_LANGUAGE', 'en');
